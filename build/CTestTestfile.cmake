af5462068cd0812834b438f558d5c84992a15bc6
BeanLovesJazz <904951400@qq.com>
2023-03-08 21:36:48 -0500

    seven dof arm is done

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/beanlovesjazz/urdf_model_ws/src
+# Build directory: /home/beanlovesjazz/urdf_model_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("mastering_ros_robot_description_pkg")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/beanlovesjazz/urdf_model_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/beanlovesjazz/ws_moveit/devel;/home/beanlovesjazz/demo03_ws/devel;/home/beanlovesjazz/demo01_ws/devel;/home/beanlovesjazz/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/beanlovesjazz/ws_moveit/devel;/home/beanlovesjazz/demo03_ws/devel;/home/beanlovesjazz/demo01_ws/devel;/home/beanlovesjazz/catkin_ws/devel;/opt/ros/noetic' != '' else []
